13ffba6d47b9086031da0c52758c9e076a9f86bc
Mahmud Burhanudin <mahmud.bin_burhanudin@kcl.ac.uk>
2023-09-30 17:42:12 +0100

    finish the code, run all test and all are green.

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if (number_of_primes <= 0):
+        raise ValueError
+    
     list = []
+
+    primeCandidate = 1
+    count = 0
+
+    while (count < number_of_primes):
+        primeCandidate += 1
+        if (isPrime(primeCandidate)):
+            list.append(primeCandidate)
+            count += 1
+
     return list
+
+
+def isPrime(number):
+    for i in range(2, number):
+        if ((number % i == 0) and i != number):
+            return False
+    return True
